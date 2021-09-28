6f8b1ed61dc23d08ca3ea2ad5815135d9d43d224
ConnerReese2024 <820449@mystma.org>
2021-09-28 09:13:53 -0500

    2

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def password(p):
+
+    if p == "Knights19":
+         return "Acces Granted"
+    
+    else: "accessdenied"
+
+print(password("Knights19"))
--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ import main;
 import datetime;
 
 year = 2021
-month = 8
-day = 8
+month = 10
+day = 10
 
 def test_code():
     assert main.password("Knights19") == "ACCESS GRANTED", "password('Knights19') == ACCESS GRANTED failed"
